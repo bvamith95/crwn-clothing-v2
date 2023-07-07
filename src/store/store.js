5db0c9a95b88f72cfc2f8b3e3af05605aed21f72
Amith BV <bvamith95@gmail.com>
2023-07-07 17:19:01 +0530

    added Redux-Thunk to handle Async Functions.

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import {compose, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 import { legacy_createStore as createStore } from "redux"
+import thunk from "redux-thunk";
 
 import logger from "redux-logger";
 
@@ -10,12 +11,12 @@ import { rootReducer } from "./root-reducer";
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
+    whitelist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(
     Boolean
     );
 
