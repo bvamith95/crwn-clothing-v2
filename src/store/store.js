b2f344e8e82801e44704f8a3376befdff504688b
Amith BV <bvamith95@gmail.com>
2023-07-13 16:19:20 +0530

    Replaced redux-thunk with redux-saga and moved all async code to sagas

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,38 @@
-import {compose, applyMiddleware } from "redux";
+import {compose,applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
-import { legacy_createStore as createStore } from "redux"
-import thunk from "redux-thunk";
-
 import logger from "redux-logger";
+import { legacy_createStore as createStore } from "redux"
+import createSagaMiddleware from 'redux-saga';
 
 import { rootReducer } from "./root-reducer";
+import { rootSaga } from './root-saga';
 
+const sagaMiddleware = createSagaMiddleware();
+
+const middleWares = [
+    process.env.NODE_ENV === 'development' && logger,
+    sagaMiddleware,
+  ].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['cart']
+    blacklist: ['user'],
 }
 
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(
-    Boolean
-    );
-
-const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
