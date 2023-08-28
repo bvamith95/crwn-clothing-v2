25a8bd03e2833aee25e5662cd1c115b91c67fc9a
Amith BV <bvamith95@gmail.com>
2023-08-28 20:12:09 +0530

    Added performance optimisations and code splitting

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,23 +1,24 @@
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useCallback } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { selectCartItems } from '../../store/cart/cart.selector';
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from "../../store/cart/cart.selector";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
-} from './cart-dropdown.styles';
+} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
-  const cartItems  = useSelector(selectCartItems);
+  const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
-    navigate('/checkout');
-  }; 
+  const goToCheckoutHandler = useCallback(() => {
+    navigate("/checkout");
+  },[]);
 
   return (
     <CartDropdownContainer>
